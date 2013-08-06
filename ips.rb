3873f0bdcd4e0a22d6f1204dd53f0f9013c23911
Sergio Aguirre <sergio@ievolutioned.com>
2013-08-05 20:42:14 -0500

    adding Ascending or Descending sort

--- ips.rb
+++ ips.rb
@@ -1,14 +1,23 @@
-class Comparison
-  def self.by_ip(a, b)
+class Comparison 	
+  def self.by_ip(a, b, asc)
     a=a.split('.')
     b=b.split('.')
-
-    4.times{  |n| return ( a[n].to_i > b[n].to_i ? -1 : 1 )   if a[n] !=  b[n]  }	
-    
+    order = asc ? 1 : -1 
+    4.times{  |n| return ( a[n].to_i > b[n].to_i ? -1*order : 1*order )   if a[n] !=  b[n]  }	
   end
+
+  def self.compare_ips(ips,asc)
+  	puts
+	puts "======"
+	ips.sort!{|x,y| Comparison.by_ip(x,y, asc.downcase == 'desc') }
+	puts "Sorted IPs #{asc.capitalize}"
+	puts
+	puts ips
+  end	
 end
 
-ips=["100.123.23.24",
+ips=[
+	"100.123.23.24",
 	"95.34.44.10",
 	"100.100.100.100",
 	"90.35.23.10",	
@@ -22,13 +31,14 @@ ips=["100.123.23.24",
 	"10.34.45.67",
 	"90.35.23.34"	
 	]
-puts ips
 
-
-ips.sort!{|x,y| Comparison.by_ip(x,y) }
 puts
+puts "======"
+puts " Original IPs"
 puts ips
 
+Comparison.compare_ips(ips,'Asc')
+Comparison.compare_ips(ips,'desc')
 
 
 
